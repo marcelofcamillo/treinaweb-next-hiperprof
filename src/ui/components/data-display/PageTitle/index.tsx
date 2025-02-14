af2efad365ccd0e2e97100beef00947ce16f0986
Marcelo Camillo <marcelocamillo18@gmail.com>
2025-02-14 20:07:07 -0300

    Criando componente PageTitle

---
+++ src/ui/components/data-display/PageTitle/index.tsx
@@ -0,0 +1,21 @@
+import {
+  PageSubtitleStyled,
+  PageTitleContainer,
+  PageTitleStyled,
+} from './styles';
+
+export interface PageTitleProps {
+  title: string;
+  subtitle?: string;
+}
+
+const PageTitle: React.FC<PageTitleProps> = ({ title, subtitle }) => {
+  return (
+    <PageTitleContainer>
+      <PageTitleStyled color={'primary'}>{title}</PageTitleStyled>
+      <PageSubtitleStyled>{subtitle}</PageSubtitleStyled>
+    </PageTitleContainer>
+  );
+};
+
+export default PageTitle;
