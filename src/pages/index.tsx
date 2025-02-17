4a1f006f8e789bee61ca03c11ecfd7de1f4baf80
Marcelo Camillo <marcelocamillo18@gmail.com>
2025-02-17 09:05:50 -0300

    Criando a página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import PageTitle from '@components/data-display/PageTitle';
+import { TextField, Icon, Button } from '@mui/material';
+import { BoxButtons, HomeContainer } from '@styles/pages/index.styles';
 
 export default function Home() {
   return (
-    <div>
-      <PageTitle title='Título' subtitle='Subtítulo' />
-    </div>
+    <HomeContainer>
+      <PageTitle
+        title='ENCONTRE O PROFESOR IDEAL PARA VOCÊ!'
+        subtitle='Pesquise pelo professor ideal para você'
+      />
+      <TextField
+        sx={{ mt: 3, mb: 1 }}
+        label={'Encontre um professor'}
+        InputProps={{ startAdornment: <Icon sx={{ mr: 1 }}>search</Icon> }}
+        fullWidth
+        required
+      />
+
+      <BoxButtons>
+        <Button variant='contained' type='submit'>
+          Buscar o professor perfeito
+        </Button>
+      </BoxButtons>
+    </HomeContainer>
   );
 }
+
+// 04:20
